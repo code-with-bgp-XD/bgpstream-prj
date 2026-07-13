d8731fcef12a35c161aecd44ba64cbef1928556b
FishTofu <giaogiao521@163.com>
2026-07-13 17:20:13 +0800

    fix

--- examples/example_announcement_counter_plugin.cpp
+++ examples/example_announcement_counter_plugin.cpp
@@ -12,7 +12,7 @@ class ExampleAnnouncementCounterProcessor : public bgpstream_runner::MessageProc
         return bgpstream_runner::BGPMessageFields::Type;
     }
 
-    //bool supports_concurrent_message_handling() const noexcept override { return true; }
+    bool supports_concurrent_message_handling() const noexcept override { return true; }
 
     void handle_messages(const std::vector<bgpstream_runner::BGPMessage> &messages) override {
         std::uint64_t announcement_messages = 0;
